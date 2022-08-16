8ad49c39cc362cf532243421ae44062b5d1d08f3
Michel Correa <micheloscbc@hotmail.com>
2022-08-16 11:56:18 -0300

    first commit

---
+++ msclientes/src/main/java/io/github/cursomicroservicos/msclientes/infra/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package io.github.cursomicroservicos.msclientes.infra.repository;
+
+import io.github.cursomicroservicos.msclientes.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    public Optional<Cliente> findByCpf(String cpf);
+}
