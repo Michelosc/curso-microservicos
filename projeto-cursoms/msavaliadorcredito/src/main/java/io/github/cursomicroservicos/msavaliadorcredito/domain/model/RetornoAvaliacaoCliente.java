660dd717aefe725578d487b5f12a2d91f0c68697
Michel Correa <micheloscbc@hotmail.com>
2022-09-12 09:43:01 -0300

    Modificando configurações para projeto rodar com modulos

---
+++ projeto-cursoms/msavaliadorcredito/src/main/java/io/github/cursomicroservicos/msavaliadorcredito/domain/model/RetornoAvaliacaoCliente.java
@@ -0,0 +1,13 @@
+package io.github.cursomicroservicos.msavaliadorcredito.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class RetornoAvaliacaoCliente {
+
+    List<CartaoAprovado> cartoes;
+}
